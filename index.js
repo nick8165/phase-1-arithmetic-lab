bc5dd83bd901a90a8d9a5e0b57fa75b3c2fcbb15
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-10-11 13:20:45 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random()* 10);
+Number.isInteger(random);
+console.log(random);
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 2, 3, 4, 20);
+console.log(max);
